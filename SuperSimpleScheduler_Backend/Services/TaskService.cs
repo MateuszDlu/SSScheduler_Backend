350d3f88a7c8a4a8eb715624e53fc12223e192bf
MateuszDlu <mateuszdluzniewski02@gmail.com>
2024-05-30 16:16:58 +0200

    adding controllers and services structure

---
+++ SuperSimpleScheduler_Backend/Services/TaskService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperSimpleScheduler_Backend.Services
+{
+    public class TaskService
+    {
+        
+    }
+}
