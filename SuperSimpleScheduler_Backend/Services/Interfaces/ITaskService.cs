ae210a4e5c8bbe77f7f5d0b623fcbbdd3eaae017
MateuszDlu <mateuszdluzniewski02@gmail.com>
2024-05-31 17:28:09 +0200

    adding service interfaces

--- SuperSimpleScheduler_Backend/Services/Interfaces/ITaskService.cs
+++ SuperSimpleScheduler_Backend/Services/Interfaces/ITaskService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SuperSimpleScheduler_Backend.Models;
 
 namespace SuperSimpleScheduler_Backend.Services
 {
     public interface ITaskService
     {
-        
+        public Task<IEnumerable<Models.Task>> GetTasksByCategoryId(int categoryId);
+        public Task<Models.Task> GetTaskById(int taskId);
+        public Task<Models.Task> DeleteTaskById(int taskId);
+        public Task<Object> CreateTask(string title, string? description, DateTime? deadline, Category category);
+        public Task<Object> UpdateTask(int taskId, string title, string? description, DateTime? deadline, Category category);
     }
 }
