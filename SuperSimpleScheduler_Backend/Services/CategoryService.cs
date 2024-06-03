eb4623ef6b25bc60e318ff86b4e9a9322138ce6f
MateuszDlu <mateuszdluzniewski02@gmail.com>
2024-06-03 19:29:08 +0200

    implementation of user service (wip)

--- SuperSimpleScheduler_Backend/Services/CategoryService.cs
+++ SuperSimpleScheduler_Backend/Services/CategoryService.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SuperSimpleScheduler_Backend.Models;
 
 namespace SuperSimpleScheduler_Backend.Services
 {
-    public class CategoryService
+    public class CategoryService : ICategoryService
     {
-        
+        public Task<object> CreateCategory(string name, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> DeleteCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetCategorieById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetCategoriesByUserId(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> UpdateCategory(int categoryId, string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- SuperSimpleScheduler_Backend/Services/TaskService.cs
+++ SuperSimpleScheduler_Backend/Services/TaskService.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SuperSimpleScheduler_Backend.Models;
 
 namespace SuperSimpleScheduler_Backend.Services
 {
-    public class TaskService
+    public class TaskService : ITaskService
     {
-        
+        public Task<object> CreateTask(string title, string? description, DateTime? deadline, Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Models.Task> DeleteTaskById(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Models.Task> GetTaskById(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Models.Task>> GetTasksByCategoryId(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> UpdateTask(int taskId, string title, string? description, DateTime? deadline, Category category)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
